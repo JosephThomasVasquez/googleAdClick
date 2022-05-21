d7a8336802b37e671ce4f998d0c42a31459d4e02
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2022-05-20 22:17:31 -0700

    Removed console logs

--- app.js
+++ app.js
@@ -3,29 +3,22 @@ const submitButton = document.getElementById("form-submit");
 let formData = {};
 
 document.addEventListener("input", ({ target }) => {
-  //   console.log(target);
-
   const validInputs = ["name", "email", "phone"];
 
   if (validInputs.includes(target.name)) {
     formData = { ...formData, [target.name]: target.value };
   }
-
-  console.log("formData:", formData);
 });
 
 submitButton.addEventListener("submit", async (e) => {
   e.preventDefault();
-  //   console.log(e);
 
   const url = "https://hooks.zapier.com/hooks/catch/12559542/bfm80ew";
-
-  const headers = new Headers({ "Content-Type": "application/json" });
 
   const response = await fetch(url, {
     method: "POST",
     body: JSON.stringify({ formData }),
   });
 
-  console.log("response", response);
+  //   console.log("response", response);
 });
